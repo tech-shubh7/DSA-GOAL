cb9da39cb4a2249506443ad5996793e5245dedb2
Shubham Patel <sp8587128@gmail.com>
2026-06-15 23:39:48 +0530

    2 patterns solved

---
+++ Looping-and-Patterns/src/ReversePyramidPattern.java
@@ -0,0 +1,14 @@
+public class ReversePyramidPattern {
+    public static void main(String[] args) {
+        int n = 5;
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < i; j++) {
+                System.out.print("  ");
+            }
+            for(int k = 0; k < 2*n - (2*i + 1); k++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
---
+++ Looping-and-Patterns/src/SolidRohmbusPattern.java
@@ -0,0 +1,14 @@
+public class SolidRohmbusPattern {
+    public static void main(String[] args) {
+        int n = 5;
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < n-i-1; j++) {
+                System.out.print("  ");
+            }
+            for(int k = 0; k < n; k++) {
+                System.out.print("*  ");
+            }
+            System.out.println();
+        }
+    }
+}
