19f41241d54c0f5ff26e559b2370b5eee61dd233
Shubham Patel <sp8587128@gmail.com>
2026-06-17 23:19:50 +0530

    patterns finished and collection framework learning started

---
+++ Looping-and-Patterns/src/ButterflyPattern.java
@@ -0,0 +1,29 @@
+public class ButterflyPattern {
+    public static void main(String[] args) {
+        int n = 5;
+        for(int i = 1; i <= n; i++) {
+            for(int j = 1; j <= i; j++) {
+                System.out.print("* ");
+            }
+            for(int k = 1; k <= 2*n - 2*i; k++) {
+                System.out.print("  ");
+            }
+            for(int l = 1; l <= i; l++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        for(int i = n; i > 0; i--) {
+            for(int j = 1; j <= i; j++) {
+                System.out.print("* ");
+            }
+            for(int k = 1; k <= 2*n - 2*i; k++) {
+                System.out.print("  ");
+            }
+            for(int l = 1; l <= i; l++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
